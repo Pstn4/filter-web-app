db81e5df4dee11aa5186ae62c4c63234068cc1d8
Pstn4 <86513929+Pstn4@users.noreply.github.com>
2023-05-18 19:03:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function setup()
 canvas=createCanvas(300,300);
 canvas.center();
 video=createCapture(VIDEO);
+video.size(300,300);
 video.hide();
 
 poseNet=ml5.poseNet(video,modelloaded);
@@ -17,10 +18,12 @@ function modelloaded()
 }
 function Gotposes(results)
 {
-    if (results.length>0)
+   
+   console.log("hello") 
+   if (results.length>0)
     {
         console.log(results);
-        nose_x=results[0].pose.nose.x;
+        nose_x=results[0].pose.nose.x-25;
         nose_y=results[0].pose.nose.y;
         console.log("nose X= "+nose_x);
         console.log("nose y= "+nose_y);
@@ -28,11 +31,12 @@ function Gotposes(results)
 }
 function preload()
 {
-
+moustache=loadImage("m.png");
 }
 function draw()
 {
 image(video,0,0,300,300);
+image(moustache,nose_x,nose_y,50,30);
 }
 function TakeSnap()
 {
